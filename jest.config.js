ecb32d2dd2520a9df3a80f84ca2ac71c2639c9da
tbs-mehak-fatima <mehak313naqvi@gmail.com>
2025-09-16 14:24:52 +0500

    Setup project

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: "node", // Set test environment, you can also use jsdom for browser tests
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
-  testMatch: ["**/test/**/*.test.js"],
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
-};
